fd00b7b055359760661846c9b85745823ebb54de
jiangjian <jiangjian199204@gmail.com>
2016-05-28 23:40:34 +0800

    add generic class

--- src/com/jiangjian/study/java/generic/GenericClassSample.java
+++ src/com/jiangjian/study/java/generic/GenericClassSample.java
@@ -2,6 +2,16 @@ package com.jiangjian.study.java.generic;
 
 public class GenericClassSample {
     public static void main(String[] args) {
+        // Pair<String, int> pair = new Pair<>();
+        // 上面被注释的代码会提示语法错误，Java类型参数不允许是基本数据类型
+        //具体原因可以参考:http://stackoverflow.com/questions/2721546/why-dont-java-generics-support-primitive-types
 
+        Pair<String, Integer> orderList = new Pair<>();
+
+        orderList.setFirst("面包");
+        orderList.setSecond(5);
+
+        System.out.println("购买：" + orderList.getFirst());
+        System.out.println("数量: " + orderList.getSecond());
     }
 }
