4c9851bbe027c0cbe023152ce008208c020c56d3
jiangjian <jiangjian199204@gmail.com>
2017-09-10 11:55:02 +0800

    add insert-sort algorithm

--- src/main/java/com/jiangjian/study/java/algorithms/InsertionSort.java
+++ src/main/java/com/jiangjian/study/java/algorithms/InsertionSort.java
@@ -1,22 +1,25 @@
 package com.jiangjian.study.java.algorithms;
 
-/**
- * Created by jiangjian on 16-10-8.
- */
+
+
 public class InsertionSort {
     public static void main(String[] args) {
         int[] a = {4, 5, 3, 9, 1};
-        for(int i = 1; i < a.length; i++) {
-            int currentValue = a[i];
-            int j = i - 1;
-            while(j >= 0 && a[j] > currentValue) {
-                a[j + 1] = a[j];
-                j--;
-            }
-            a[j + 1]  = currentValue;
-        }
+        insertSort(a);
         for(int value : a) {
             System.out.print(value + ",");
+        }
+    }
+
+    public static void insertSort(int[] data) {
+        for(int i = 1; i < data.length; i++) {
+            int temp = data[i];
+            int j = i -1;
+            while(j >= 0 && temp < data[j]) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = temp;
         }
     }
 }
--- src/test/java/com/jiangjian/leanring/java/Demo.java
+++ src/test/java/com/jiangjian/leanring/java/Demo.java
@@ -3,6 +3,14 @@ package com.jiangjian.leanring.java;
 import org.junit.Test;
 
 public class Demo {
+	public Demo() {
+		System.out.println("constructor");
+	}
+
+	@Test
+	public void a() {
+		System.out.println("a");
+	}
 	@Test
 	public void test() {
 		System.out.println("aaa");
