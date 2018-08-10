f1c85f20955571f27ad527a384f1386054a60319
jiangjian <jiangjian199204@gmail.com>
2018-08-10 11:47:44 +0800

    add float demo

---
+++ src/main/java/com/jiangjian/study/java/advanced/jvm/basic/floatarthmetic/FloatArthmeticDemo.java
@@ -0,0 +1,8 @@
+package com.jiangjian.study.java.advanced.jvm.basic.floatarthmetic;
+
+public class FloatArthmeticDemo {
+    public static void main(String[] args) {
+        float a = 1.0f;
+        System.out.println(Float.toHexString(a));
+    }
+}
