76e17bd14caeaf4ddfdf6b5c56e4604bb1537e13
jiangjian <jiangjian199204@gmail.com>
2018-08-10 10:44:19 +0800

    add jvm integer arthmetic

---
+++ src/main/java/com/jiangjian/study/java/advanced/jvm/basic/typeconversion/ByteArithmetic.java
@@ -0,0 +1,9 @@
+package com.jiangjian.study.java.advanced.jvm.basic.typeconversion;
+
+public class ByteArithmetic {
+    public static void main(String[] args) {
+        short a = 0;
+        short b = 1;
+        short c = (short)(a + b);
+    }
+}
