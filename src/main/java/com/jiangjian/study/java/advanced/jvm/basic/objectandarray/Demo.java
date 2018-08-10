4a28026f592ca8cd8df2193ed0d8ab5edbc7266c
jiangjian <jiangjian199204@gmail.com>
2018-08-10 13:09:05 +0800

    [jvm] object and array create

---
+++ src/main/java/com/jiangjian/study/java/advanced/jvm/basic/objectandarray/Demo.java
@@ -0,0 +1,12 @@
+package com.jiangjian.study.java.advanced.jvm.basic.objectandarray;
+
+public class Demo {
+    public static void main(String[] args) {
+        Integer a = new Integer(10);
+
+        int[] b = new int[2];
+        Integer[] c = new Integer[2];
+        Integer[][][][] d = new Integer[2][2][2][2];
+        Integer e = d[0][0][0][0];
+    }
+}
