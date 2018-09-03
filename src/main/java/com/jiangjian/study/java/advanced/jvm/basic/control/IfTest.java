445c7a8d0a2ab6dff72a19f1dfaa311783b92391
jiangjian <jiangjian199204@gmail.com>
2018-09-03 10:41:47 +0800

    [jvm]if control

---
+++ src/main/java/com/jiangjian/study/java/advanced/jvm/basic/control/IfTest.java
@@ -0,0 +1,13 @@
+package com.jiangjian.study.java.advanced.jvm.basic.control;
+
+/**
+ * @author jiangjian
+ */
+public class IfTest {
+    public static void main(String[] args) {
+        boolean c = true;
+        if(c) {
+            System.out.println("true");
+        }
+    }
+}
